b00c210d064f11fe8dbb1f5d4b3b002137efa32b
spadaritucks <thiago.spadari02@gmail.com>
2024-12-16 07:19:03 -0300

    commit

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,6 +4,9 @@ import './page.scss'
 import homeImage from '@/../public/home-image.jpg'
 import Link from "next/link";
 import whatsapp from '@/../public/whatsaap.png'
+import MusicNoteIcon from '@mui/icons-material/MusicNote';
+import GavelIcon from '@mui/icons-material/Gavel';
+import logo from '@/../public/logo.png'
 
 export default function Home() {
 
@@ -23,39 +26,40 @@ export default function Home() {
         <h1>Area de Atuação</h1>
         <div className="area-atuacao-informations">
           <div className="container-information">
-            <h2>Direito Digital</h2>
+            <GavelIcon sx={{ fontSize: 60 }} />
+            <h2>Direito de Sincronização</h2>
+            <p>Refere-se ao direito proveniente da combinação de sons e imagens, 
+              permitindo que os criadores de músicas recebam uma remuneração específica 
+              pela utilização de suas criações.</p>
+
+          </div>
+          <div className="container-information">
+            <GavelIcon sx={{ fontSize: 60 }} />
+            <h2>Direito de Execução Pública</h2>
+            <p>Refere-se ao direito derivado da comunicação pública de obras musicais e audiovisuais, 
+              sendo sua arrecadação e distribuição realizadas pelo ECAD – Escritório Central de Arrecadação e Distribuição.</p>
+
+          </div>
+          <div className="container-information">
+            <GavelIcon sx={{ fontSize: 60 }} />
+            <h2>Assessoria e Consultoria Jurídica na Área dos Direitos Autorais</h2>
             <p>É uma espécie de direito gerado pela comercialização e disponibilização do acervo  no ambiente virtual,
               em suas mais diversas variações.</p>
 
           </div>
           <div className="container-information">
-            <h2>Direito Digital</h2>
-            <p>É uma espécie de direito gerado pela comercialização e disponibilização do acervo  no ambiente virtual,
-              em suas mais diversas variações.</p>
+            <GavelIcon sx={{ fontSize: 60 }} />
+            <h2>Direito Fonomecânico</h2>
+            <p>Corresponde ao direito dos compositores pela fixação ou gravação de suas obras em suportes físicos 
+              (como CDs, DVDs e Blu-rays) e pela posterior comercialização desses produtos.</p>
 
           </div>
-          <div className="container-information">
+          <div className="container-information" style={{gridColumn: '1/-1'}}>
+            <GavelIcon sx={{ fontSize: 60 }} />
             <h2>Direito Digital</h2>
-            <p>É uma espécie de direito gerado pela comercialização e disponibilização do acervo  no ambiente virtual,
-              em suas mais diversas variações.</p>
-
-          </div>
-          <div className="container-information">
-            <h2>Direito Digital</h2>
-            <p>É uma espécie de direito gerado pela comercialização e disponibilização do acervo  no ambiente virtual,
-              em suas mais diversas variações.</p>
-
-          </div>
-          <div className="container-information">
-            <h2>Direito Digital</h2>
-            <p>É uma espécie de direito gerado pela comercialização e disponibilização do acervo  no ambiente virtual,
-              em suas mais diversas variações.</p>
-
-          </div>
-          <div className="container-information">
-            <h2>Direito Digital</h2>
-            <p>É uma espécie de direito gerado pela comercialização e disponibilização do acervo  no ambiente virtual,
-              em suas mais diversas variações.</p>
+            <p>Trata-se de um tipo de direito gerado pela comercialização e disponibilização de acervos no ambiente virtual, 
+              abrangendo diversas formas de exploração.
+              </p>
 
           </div>
         </div>
@@ -63,22 +67,24 @@ export default function Home() {
       <section className="sobre-mim" id="sobre-mim">
         <h1>Sobre-mim</h1>
         <div className="sobre-mim-informations">
-          <Image className="profissional-image" src="" alt="" width={300} height={300}></Image>
+          <Image className="profissional-image" src={logo} alt="" width={300} height={300}></Image>
           <div className="sobre-mim-text">
             <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Commodi itaque cupiditate tempore.
               Esse, quo enim unde ipsa quidem officiis, quibusdam a placeat similique laborum necessitatibus
               odio ad pariatur cupiditate reprehenderit.</p>
-              <small className="assinatura"> - Doutora Ana Claudia</small>
+            <small className="assinatura"> - Doutora Ana Claudia</small>
           </div>
 
         </div>
         <div className="sobre-mim-formacao-academia">
           <h1>Formação Academica</h1>
           <ul className="formacoes">
-            <li>Graduação em Direito</li>
-            <li>Graduação em Ciências Contábeis</li>
-            <li>Graduação em Administração</li>
-            <li>Ingles Avançado</li>
+            <li>Formação direito em 1995 Faculdade de Direito São Bernardo do Campo </li>
+            <li>Especialização direito autoral FGV</li>
+            <li>Curso básico direito autoral marcas e patentes OMPI ( Organização Mundial de Propriedade Intelectual)</li>
+            <li>Especialização avançado de direito autoral OMPI</li>
+            <li>Especialização avançado de marcas OMPI</li>
+            <li>Por graduação direito autoral OMPI</li>
           </ul>
         </div>
       </section>
--- src/components/navbar/component.tsx
+++ src/components/navbar/component.tsx
@@ -1,20 +1,34 @@
 'use client'
 
 import Link from "next/link"
+import MenuIcon from '@mui/icons-material/Menu';
+import CloseIcon from '@mui/icons-material/Close';
 import './component.scss'
+import { useState } from "react";
+import Image from "next/image";
+import logo from '@/../public/logo.png'
 
 
 export default function Navbar () {
 
+    const [toogleOpen, setToogleOpen] = useState<boolean>(false)
+
+    const handleOpenMenu = () => {
+        setToogleOpen(!toogleOpen)
+    }
+
     return(
         <nav className="nav-area">
-            <h1>Dra Ana Claudia</h1>
-            <ul className="nav-links">
+            <Image src={logo} width={70} height={70} alt="Logo" className="logo" />
+            <ul className={`nav-links ${toogleOpen ? 'open': ''}`}>
                 <li><Link href='#home' >Home</Link></li>
                 <li><Link href='#area-de-atuacao' >Area de Atuação</Link></li>
                 <li><Link href='#sobre-mim' >Sobre mim</Link></li>
                 <li><Link href='#projetos-artigos' >Projetos e Artigos</Link></li>
             </ul>
+            
+            {toogleOpen ? <div className="hamburguer-menu" onClick={handleOpenMenu}><CloseIcon sx={{fontSize: 50}}/></div> : 
+            <div className="hamburguer-menu" onClick={handleOpenMenu}><MenuIcon sx={{fontSize: 50}}/></div>}
         </nav>
     )
 }
